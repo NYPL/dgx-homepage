951ffe3d93acc1f1f533e596934a7bc61cc604ba
Sean Redmond <github-smr@sneakemail.com>
2015-11-30 14:50:03 -0500

    Lint'd

--- src/app/components/Application/Application.jsx
+++ src/app/components/Application/Application.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-
 import Store from '../../stores/Store.js';
 import HomepageRow from 'dgx-homepage-row-component';
 import BlogFeatures from 'dgx-blog-features-component';
 import HomepageStaffPicks from 'dgx-homepage-staff-picks-component';
 import BooklistWidget from 'dgx-booklist-component';
-
 import Header from 'dgx-header-component';
 import FeatureRow from 'dgx-feature-row-component';
 import { SeeMoreButton } from 'dgx-react-buttons';
@@ -16,90 +14,89 @@ class App extends React.Component {
 
     this.state = Store.getState();
   }
-  
+
   render() {
     return (
-      <div className='app-wrapper'>
+      <div className="app-wrapper">
         <Header />
 
-	<HomepageRow
-	title={'Learn Something New'}
-	sideBar={
-	    <div>
+        <HomepageRow
+        title={'Learn Something New'}
+        sideBar={
+            <div>
             <h2>Learn Something New</h2>
-            <SeeMoreButton label='See More...'
+            <SeeMoreButton label="See More..."
             target={"/events/classes/calendar"} />
             </div>
-	} content={<FeatureRow
-	    name={'HP-Lear'}
-	    id={'HP-Lears'}
-	    className={'RightColumn'}
-	    items={[]} />
-	} />
+        } content={<FeatureRow
+            name={'HP-Lear'}
+            id={'HP-Lears'}
+            className={'RightColumn'}
+            items={[]} />
+        } />
 
-	<HomepageRow
-	title={'Staff Picks'}
-	sideBar={
-	    <div>
+        <HomepageRow
+        title={'Staff Picks'}
+        sideBar={
+            <div>
             <h2>Staff Picks</h2>
-            <SeeMoreButton label='See More...'
+            <SeeMoreButton label="See More..."
             target={"/staff-picks"} />
             </div>
-	} content={
-      	    <HomepageStaffPicks
-	    name={'HP-StaffPicks'}
-	    id={'HP-StaffPicks'}
-	    className={'RightColumn'}
-	    items={[]} />
-	} />
+        } content={
+            <HomepageStaffPicks
+            name={'HP-StaffPicks'}
+            id={'HP-StaffPicks'}
+            className={'RightColumn'}
+            items={[]} />
+        } />
 
-	<HomepageRow
-	title={'Books We Love'}
-	sideBar={
-	    <div>
+        <HomepageRow
+        title={'Books We Love'}
+        sideBar={
+            <div>
             <h2>Books We Love</h2>
-            <SeeMoreButton label='See More...'
+            <SeeMoreButton label="See More..."
             target={"//nypl.org/blog"} />
             </div>
-	} content={
-	    <BooklistWidget
-	    name={'HP-Booklist'}
-	    id={'HP-Booklist'}
-	    className={'RightColumn'}
-	    items={[]} />
-	} />
+        } content={
+            <BooklistWidget
+            name={'HP-Booklist'}
+            id={'HP-Booklist'}
+            className={'RightColumn'}
+            items={[]} />
+        } />
 
-	<HomepageRow
-	title={'From Our Blog'}
-	sideBar={
-	    <div>
+        <HomepageRow
+        title={'From Our Blog'}
+        sideBar={
+            <div>
             <h2>From Our Blog</h2>
-            <SeeMoreButton label='See More...'
+            <SeeMoreButton label="See More..."
             target={"//nypl.org/blog"} />
             </div>
-	} content={
-	    <BlogFeatures
-	    name={'HP-Blogs'}
-	    id={'HP-Blogs'}
-	    className={'RightColumn'}
-	    items={[]} />
-	} />
+        } content={
+            <BlogFeatures
+            name={'HP-Blogs'}
+            id={'HP-Blogs'}
+            className={'RightColumn'}
+            items={[]} />
+        } />
 
-	<HomepageRow
-	title={'Of Note'}
-	sideBar={
-	    <div>
+        <HomepageRow
+        title={'Of Note'}
+        sideBar={
+            <div>
             <h2>Of Note</h2>
-            <SeeMoreButton label='See More...'
+            <SeeMoreButton label="See More..."
             target={"https://encrypted.google.com/search?q=good+data"} />
             </div>
-	} content={<FeatureRow
-	    name={'HP-OfNote'}
-	    id={'HP-OfNote'}
-	    className={'RightColumn'}
-	    items={[]} />
-	} />
-	
+        } content={<FeatureRow
+            name={'HP-OfNote'}
+            id={'HP-OfNote'}
+            className={'RightColumn'}
+            items={[]} />
+        } />
       </div>
     );
   }
