1f6e4f2e8e6b5e940ecb1c6a7d9e821a4487d6f0
Kang Peng <kangtingpeng@nypl.org>
2016-02-04 17:30:51 -0500

    Updated id to camelCase.

--- src/app/components/Application/Application.jsx
+++ src/app/components/Application/Application.jsx
@@ -77,7 +77,7 @@ class App extends React.Component {
             title={'Learn Something New'}
             link={'/events/classes/calendar'}
             seeMoreStyle={styles.mobileBtn}
-            seeMoreId='learn-SeeMore'
+            seeMoreId='learnSeeMore'
             content={
               <FeatureRow
                 name={'hpLearn'}
@@ -130,7 +130,7 @@ class App extends React.Component {
             title={'Of Note'}
             className='ofNoteRow homepageRow'
             seeMoreStyle={styles.mobileBtn}
-            seeMoreId='ofNote-SeeMore'
+            seeMoreId='ofNoteSeeMore'
             content={
               <FeatureRow
                 id={'hpOfNote'}
