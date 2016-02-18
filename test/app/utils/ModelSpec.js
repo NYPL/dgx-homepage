7546b09db1b2ab836e28a793203ce62dec8cd8eb
Kang Peng <kangtingpeng@nypl.org>
2016-02-18 11:16:34 -0500

    Added unit test settings.

---
+++ test/app/utils/ModelSpec.js
@@ -0,0 +1,41 @@
+var React = require('react');
+var TestUtils = require('react/lib/ReactTestUtils');
+var _ = require('underscore');
+
+describe('NYPL Homepage Utils Unit Tests', function () {
+  var build;
+
+  /*
+   * dayFormatUppercase
+   *   The input is a string of the name of the weekday.
+   *
+   *   Returns the name in AP style with uppercase.
+   */
+  describe('Model: build', function () {
+    console.log('>>>>>test<<<<<');
+    // beforeEach(inject(function (_build_) {
+    //   build = _build_;
+    // }));
+
+    // it('should have a dayFormatUppercase function', function () {
+    //   expect(dayFormatUppercaseFilter).toBeDefined();
+    //   expect(angular.isFunction(dayFormatUppercaseFilter)).toBe(true);
+    // });
+
+    // it('should convert the names of week day into AP style', function () {
+    //   expect(build('')).toEqual('');
+      // expect(dayFormatUppercaseFilter('Tue.')).toEqual('TUES');
+      // expect(dayFormatUppercaseFilter('Thu.')).toEqual('THURS');
+    // });
+
+    // it('should be an empty string if input is NOT a name of weekday', function () {
+    //   expect(dayFormatUppercaseFilter('Banana')).toEqual('');
+    //   expect(dayFormatUppercaseFilter()).toBeFalsy();
+    // });
+
+    // it('should be an empty string if input is NOT given', function () {
+    //   expect(dayFormatUppercaseFilter()).toEqual('');
+    //   expect(dayFormatUppercaseFilter()).toBeFalsy();
+    // });
+  });
+});
