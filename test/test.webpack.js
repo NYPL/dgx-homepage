30d8b609a5d32e8ffb92b432118db6249ad79661
Kang Peng <kangtingpeng@nypl.org>
2016-04-15 14:32:27 -0400

    Imported mocha.

--- test/test.webpack.js
+++ test/test.webpack.js
@@ -1,3 +1,3 @@
-var context = require.context('.', true, /.+\Spec\.jsx?$/);
-context.keys().forEach(context);
-module.exports = context;
+// var context = require.context('.', true, /.+\Spec\.jsx?$/);
+// context.keys().forEach(context);
+// module.exports = context;
