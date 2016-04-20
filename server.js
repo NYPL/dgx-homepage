b669317f7b9dad308186e0f41a80e389b8270ab1
Kang Peng <kangtingpeng@nypl.org>
2016-04-20 12:16:16 -0400

    Updated server.js following eslint.

--- server.js
+++ server.js
@@ -24,11 +24,9 @@ const INDEX_PATH = path.resolve(ROOT_PATH, 'src/client');
 const DIST_PATH = path.resolve(ROOT_PATH, 'dist');
 const VIEWS_PATH = path.resolve(ROOT_PATH, 'src/views');
 const WEBPACK_DEV_PORT = appConfig.webpackDevServerPort || 3000;
-
-let isProduction = process.env.NODE_ENV === 'production';
-  // Assign API Routes
-
-let app = express();
+const isProduction = process.env.NODE_ENV === 'production';
+// Assign API Routes
+const app = express();
 
 app.use(compress());
 
@@ -52,56 +50,53 @@ app.use('*/src/client', express.static(INDEX_PATH));
 app.use('/', apiRoutes);
 
 app.get('/', (req, res) => {
-  let app, iso;
-
-  iso = new Iso();
+  const iso = new Iso();
 
   alt.bootstrap(JSON.stringify(res.locals.data || {}));
 
-  app = React.renderToString(React.createElement(Application));
+  const appString = React.renderToString(React.createElement(Application));
 
-  iso.add(app, alt.flush());
+  iso.add(appString, alt.flush());
 
   // First parameter references the ejs filename
   res.render('index', {
     app: iso.render(),
     appTitle: appConfig.appTitle,
     favicon: appConfig.favIconPath,
-    isProduction: isProduction,
+    isProduction,
     gaCode: analytics.google.code(isProduction),
     webpackPort: WEBPACK_DEV_PORT,
     appEnv: process.env.APP_ENV || 'no APP_ENV available - using production',
-    apiUrl: res.locals.data.completeApiUrl
+    apiUrl: res.locals.data.completeApiUrl,
   });
-
 });
 
-let server = app.listen(app.get('port'), (error, result) => {
+const server = app.listen(app.get('port'), (error) => {
   if (error) {
     console.log(colors.red(error));
   }
 
   console.log(colors.yellow.underline(appConfig.appName));
   console.log(colors.green('Express server is listening at'),
-    colors.cyan('localhost:' + app.get('port')));
+    colors.cyan(`localhost:${app.get('port')}`));
 });
 
 // This function is called when you want the server to die gracefully
 // i.e. wait for existing connections
-let gracefulShutdown = function() {
+const gracefulShutdown = () => {
   console.log('Received kill signal, shutting down gracefully.');
-  server.close(function() {
+  server.close(() => {
     console.log('Closed out remaining connections.');
     process.exit(0);
-  }); 
-  // if after 
-  setTimeout(function() {
+  });
+  // if after
+  setTimeout(() => {
     console.error('Could not close connections in time, forcefully shutting' +
       'down');
-    process.exit()
+    process.exit();
   }, 1000);
-}
-// listen for TERM signal .e.g. kill 
+};
+// listen for TERM signal .e.g. kill
 process.on('SIGTERM', gracefulShutdown);
 // listen for INT signal e.g. Ctrl-C
 process.on('SIGINT', gracefulShutdown);
@@ -119,9 +114,9 @@ if (!isProduction) {
     historyApiFallback: true,
     headers: {
       'Access-Control-Allow-Origin': 'http://localhost:3001',
-      'Access-Control-Allow-Headers': 'X-Requested-With'
-    }
-  }).listen(3000, 'localhost', (error, result) => {
+      'Access-Control-Allow-Headers': 'X-Requested-With',
+    },
+  }).listen(3000, 'localhost', (error) => {
     if (error) {
       console.log(colors.red(error));
     }
