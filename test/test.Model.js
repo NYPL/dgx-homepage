828ab3524a67c9a6919d9e974d9fef19824a4bc7
Kang Peng <kangtingpeng@nypl.org>
2016-04-20 16:43:28 -0400

    Removed unnecessary checks in Model.js and also related tests.

--- test/test.Model.js
+++ test/test.Model.js
@@ -53,22 +53,6 @@ describe('NYPL Homepage Model Unit Test: ', () => {
         defaultModelAppData
       );
     });
-
-    it('should return default data skeleton if the input is not an array', () => {
-      expect(modelAppData({}, defaultModelAppData)).to.equal(
-        defaultModelAppData
-      );
-    });
-
-    it('should return default data skeleton if the input is not an array', () => {
-      expect(modelAppData('', defaultModelAppData)).to.equal(
-        defaultModelAppData
-      );
-    });
-
-    it('should return default data skeleton if the input is an empty array', () => {
-      expect(modelAppData([], defaultModelAppData)).to.equal(defaultModelAppData);
-    });
   });
 
   /** Model.assignComponentName
