b86c410473a5c92761bdbd89334564962e02e962
Edwin Guzman <edwin.gzmn@gmail.com>
2016-08-31 10:19:38 -0400

    Removing and updating axios call for homepage data.

--- src/server/ApiRoutes/ApiRoutes.js
+++ src/server/ApiRoutes/ApiRoutes.js
@@ -4,16 +4,13 @@ import parser from 'jsonapi-parserinator';
 
 import { navConfig } from 'dgx-header-component';
 
-import Model from 'dgx-model-data';
 import HomepageModel from '../../app/utils/Model.js';
 import config from '../../../appConfig.js';
 
-const { HeaderItemModel } = Model;
-const { api, homepageApi, headerApi } = config;
+const { api, homepageApi } = config;
 const router = express.Router();
 const appEnvironment = process.env.APP_ENV || 'production';
 const apiRoot = api.root[appEnvironment];
-const headerOptions = createOptions(headerApi);
 const homepageOptions = createOptions(homepageApi);
 
 function createOptions(api) {
@@ -28,23 +25,17 @@ function fetchApiData(url) {
   return axios.get(url);
 }
 
-function getHeaderData() {
-  const headerApiUrl = parser.getCompleteApi(headerOptions);
-  return fetchApiData(headerApiUrl);
-}
-
 function HomepageApp(req, res, next) {
   const homepageApiUrl = parser.getCompleteApi(homepageOptions);
 
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Cache-Control', 'max-age=3600');
 
-  axios.all([getHeaderData(), fetchApiData(homepageApiUrl)])
-    .then(axios.spread((headerData, homepageData) => {
+  axios
+    .get(homepageApiUrl)
+    .then(homepageData => {
       const homepageParsed = parser.parse(homepageData.data, homepageOptions);
       const homepageModelData = HomepageModel.build(homepageParsed);
-      const headerParsed = parser.parse(headerData.data, headerOptions);
-      const headerModelData = HeaderItemModel.build(headerParsed);
 
       res.locals.data = {
         HomepageStore: {
@@ -61,16 +52,13 @@ function HomepageApp(req, res, next) {
           carouselIndexValue: 0,
           whatsHappeningIndexValue: 0,
         },
-        HeaderStore: {
-          headerData: navConfig.current,
-        },
         // Set the API URL here so we can access it when we
         // render in the EJS file.
         completeApiUrl: '',
       };
 
       next();
-    }))
+    })
     .catch(error => {
       console.log(`error calling API : ${error}`);
       console.log(`Attempted to call : ${homepageApiUrl}`);
