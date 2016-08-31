4aac78d63e343e945a9ee8f439f0eaad4d7432ca
Rafael Hernandez <rafaelhernandez@nypl.org>
2016-08-31 11:21:22 -0400

    Updated Homepage App removing some Header configuration and ESlinting 100% pass. Removed npm modules that were not used at all by the Homepage

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,24 +2,15 @@ import React from 'react';
 import { render } from 'react-dom';
 import alt from 'dgx-alt-center';
 import Iso from 'iso';
-import FeatureFlags from 'dgx-feature-flags';
-
 // Homepage App
 import App from '../app/components/Application/Application.jsx';
-
 // Analytics
-import ga from 'react-ga';
-import { config } from 'dgx-react-ga';
-
+// import ga from 'react-ga';
+// import { config } from 'dgx-react-ga';
 // Styles
 import './styles/main.scss';
 
 window.onload = () => {
-  // Assign the FeatureFlags utility methods to the Global window object
-  if (!window.dgxFeatureFlags) {
-    window.dgxFeatureFlags = FeatureFlags.utils;
-  }
-
   // if (!window.ga) {
   //   const gaOpts = { debug: true };
   //   // Passing in false for the dev GA code
