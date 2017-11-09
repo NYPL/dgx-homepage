b8e43def6b092b9c6b4ed768c7e9f9e1c1d15a37
Kang Peng <kangtingpeng@nypl.org>
2017-11-08 19:09:56 -0500

    Updated ga init function.

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -6,6 +6,7 @@ import { render } from 'react-dom';
 import alt from 'dgx-alt-center';
 import Iso from 'iso';
 import FeatureFlags from 'dgx-feature-flags';
+import { gaUtils } from 'dgx-react-ga';
 
 // Homepage App
 import App from '../app/components/Application/Application.jsx';
@@ -16,6 +17,12 @@ import './styles/main.scss';
 window.onload = () => {
   if (!window.dgxFeatureFlags) {
     window.dgxFeatureFlags = FeatureFlags.utils;
+  }
+
+  if (!window.ga) {
+    const gaOpts = { debug: false, titleCase: false };
+
+    gaUtils.initialize('UA-1420324-3', gaOpts);
   }
 
   // Render Isomorphically
