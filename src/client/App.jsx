60a0d3554bd4507e14681cf8a9b033cf8cda87b9
Kang Peng <kangtingpeng@nypl.org>
2017-11-14 09:21:19 -0500

    Merged from development branch.

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -6,6 +6,7 @@ import { render } from 'react-dom';
 import alt from 'dgx-alt-center';
 import Iso from 'iso';
 import FeatureFlags from 'dgx-feature-flags';
+import { config, gaUtils } from 'dgx-react-ga';
 
 // Homepage App
 import App from '../app/components/Application/Application.jsx';
@@ -18,9 +19,18 @@ window.onload = () => {
     window.dgxFeatureFlags = FeatureFlags.utils;
   }
 
+  if (!window.ga) {
+    const isProd = process.env.NODE_ENV === 'production';
+    const gaOpts = { debug: !isProd, titleCase: false };
+
+    gaUtils.initialize(config.google.code(isProd), gaOpts);
+  }
+
   // Render Isomorphically
   Iso.bootstrap((state, container) => {
     alt.bootstrap(state);
     render(<App />, container);
   });
+
+  gaUtils.trackPageview('send', 'pageview', window.location.pathname);
 };
