8b2f61ea067419211b3b467a263e1e3fa536b18a
Edwin Guzman <edwin.gzmn@gmail.com>
2018-10-16 12:30:29 -0400

    Removing onclick for testing.

--- src/app/components/ContentBanner/TextItem.jsx
+++ src/app/components/ContentBanner/TextItem.jsx
@@ -77,7 +77,6 @@ const TextItem = ({
         <div className="seeMoreLink-container">
           <div
             className="seeMoreLink"
-            onClick={gaClickEvent ? (e) => gaClickEvent('Hero button', content.url, e) : null}
           >
             {buttonTextElm}
             <RightArrowIcon ariaHidden />
