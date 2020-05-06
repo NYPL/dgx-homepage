c8fe39925debebab8988129ab58170cecc149a99
Matt Misselbeck <matt@misselbeck.com>
2020-05-06 17:49:20 -0400

    RENO-1317: Setup NYPL from Home content data in structure component expects

---
+++ src/app/stores/NYPLfromHomeStore.js
@@ -0,0 +1,158 @@
+const NYPLfromHomeData = {
+  name:
+  {
+    type: 'text-group',
+    en: { text: 'NYPL from Home', type: 'text-single' }
+  },
+  children: [],
+  slots: [
+    {
+      title: { type: 'text-group', en: { text: "Get a Digital Library Card Today", type: "text-single" } },
+      image: {
+        rectangularImage: {
+          alt: "Closeup of someone holding an NYPL library card.",
+          description: "",
+          'full-uri': "https://d2720ur5668dri.cloudfront.net/sites/default/files/styles/extralarge/public/sites/default/files/get_a_digital_library_card_today_cr.jpeg",
+          type: "uri",
+        }
+      },
+      link: 'https://www.nypl.org/library-card',
+    },
+    {
+      title: { type: 'text-group', en: { text: "E-Books & More with SimplyE", type: "text-single" } },
+      image: {
+        rectangularImage: {
+          alt: "Closeup of SimplyE interface on a phone screen.",
+          description: "",
+          'full-uri': "https://d2720ur5668dri.cloudfront.net/sites/default/files/styles/extralarge/public/sites/default/files/ebooks_more_with_simplye_cr.jpeg",
+          type: "uri",
+        }
+      },
+      link: 'https://www.nypl.org/books-music-movies/ebookcentral/simplye',
+    },
+    {
+      title: { type: 'text-group', en: { text: "Online Classes & Events", type: "text-single" } },
+      image: {
+        rectangularImage: {
+          alt: "Woman smiling towards camera while using a computer",
+          description: "",
+          'full-uri': "https://d2720ur5668dri.cloudfront.net/sites/default/files/styles/extralarge/public/sites/default/files/online_classes_events_cr.jpeg",
+          type: "uri",
+        }
+      },
+      link: 'https://www.nypl.org/events/calendar/online',
+    },
+    {
+      title: { type: 'text-group', en: { text: "NYPL en Español & More Multilingual Resources", type: "text-single" } },
+      image: {
+        rectangularImage: {
+          alt: "Man and woman filling out forms in library work area.",
+          description: "",
+          'full-uri': "https://d2720ur5668dri.cloudfront.net/sites/default/files/styles/extralarge/public/sites/default/files/multilingual_resources_cr.jpeg",
+          type: "uri",
+        }
+      },
+      link: 'https://www.nypl.org/about/multilingual-resources',
+    },
+    {
+      title: { type: 'text-group', en: { text: "Online Learning & Skills Training for Adults", type: "text-single" } },
+      image: {
+        rectangularImage: {
+          alt: "Trainer and student working on a tablet computer together.",
+          description: "",
+          'full-uri': "https://d2720ur5668dri.cloudfront.net/sites/default/files/styles/extralarge/public/sites/default/files/online_learning_skills_training_for_adults_cr.jpeg",
+          type: "uri",
+        }
+      },
+      link: 'https://www.nypl.org/education/adults',
+    },
+    {
+      title: { type: 'text-group', en: { text: "Remote Learning Support: Kids, Teens & Educators", type: "text-single" } },
+      image: {
+        rectangularImage: {
+          alt: "Smiling child doing school homework.",
+          description: "",
+          'full-uri': "https://d2720ur5668dri.cloudfront.net/sites/default/files/styles/extralarge/public/sites/default/files/remote_learning_support_kids_teens_educators_cr.jpg",
+          type: "uri",
+        }
+      },
+      link: 'https://www.nypl.org/education',
+    },
+    {
+      title: { type: 'text-group', en: { text: "Remote Research at NYPL", type: "text-single" } },
+      image: {
+        rectangularImage: {
+          alt: "Computer and a stack of books.",
+          description: "",
+          'full-uri': "https://d2720ur5668dri.cloudfront.net/sites/default/files/styles/extralarge/public/sites/default/files/remote_research_at_NYPL_cr.jpeg",
+          type: "uri",
+        }
+      },
+      link: 'https://www.nypl.org/research',
+    },
+    {
+      title: { type: 'text-group', en: { text: "Newspapers, Magazines & Databases", type: "text-single" } },
+      image: {
+        rectangularImage: {
+          alt: "Woman reading a magazine.",
+          description: "",
+          'full-uri': "https://d2720ur5668dri.cloudfront.net/sites/default/files/styles/extralarge/public/sites/default/files/newspapers_magazines_databases_cr.jpeg",
+          type: "uri",
+        }
+      },
+      link: 'https://www.nypl.org/newspapers-magazines-more',
+    },
+    {
+      title: { type: 'text-group', en: { text: "COVID-19: Community Support Resources", type: "text-single" } },
+      image: {
+        rectangularImage: {
+          alt: "Group of women having a discussion.",
+          description: "",
+          'full-uri': "https://d2720ur5668dri.cloudfront.net/sites/default/files/styles/extralarge/public/sites/default/files/covid_19_community_support_resources_cr.jpeg",
+          type: "uri",
+        }
+      },
+      link: 'https://www.nypl.org/get-help/community-resources',
+    },
+    {
+      title: { type: 'text-group', en: { text: "Remote Accessible Resources", type: "text-single" } },
+      image: {
+        rectangularImage: {
+          alt: "Librarian helping a young man with a tablet computer.",
+          description: "",
+          'full-uri': "https://d2720ur5668dri.cloudfront.net/sites/default/files/styles/extralarge/public/sites/default/files/remote_accessible_resources_cr.jpeg",
+          type: "uri",
+        }
+      },
+      link: 'https://www.nypl.org/accessibility',
+    },
+    {
+      title: { type: 'text-group', en: { text: "Get Help: Ask NYPL", type: "text-single" } },
+      image: {
+        rectangularImage: {
+          alt: "Ask a Librarian spelled out in a lightbox with glowing, multi-colored pegs.",
+          description: "",
+          'full-uri': "https://d2720ur5668dri.cloudfront.net/sites/default/files/styles/extralarge/public/sites/default/files/get_help_ask_NYPL_cr.jpeg",
+          type: "uri",
+        }
+      },
+      link: 'https://www.nypl.org/get-help',
+    },
+    {
+      title: { type: 'text-group', en: { text: "Get NYPL Updates", type: "text-single" } },
+      image: {
+        rectangularImage: {
+          alt: "Closeup of the NYPL lion statue.",
+          description: "",
+          'full-uri': "https://d2720ur5668dri.cloudfront.net/sites/default/files/styles/extralarge/public/sites/default/files/get_NYPL_updates_cr.jpeg",
+          type: "uri",
+        }
+      },
+      link: 'https://www.nypl.org/get-help',
+    }
+  ],
+  slug: 'learn-something-new',
+  link: '/education'
+}
+
+module.exports = NYPLfromHomeData;
